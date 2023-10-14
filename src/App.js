05287e96b6653e2287f35f8476efaa2736c20570
rabahdouasii@yahoo.fr <rabahdouassi@yahoo.fr>
2023-10-14 20:29:38 +0200

    Changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> ca va rabahDDE.
+          Edit <code>src/App.js</code> ca va rabah BIEN.
         </p>
         <a
           className="App-link"
